2cba0ee68d4ed5f526c7443850b740d791558c19
Shoumodip Kar <shoumodipkar@gmail.com>
2021-09-12 11:51:16 +0530

    More robust key input handling

--- src/main.c
+++ src/main.c
@@ -11,7 +11,14 @@
 float time = 0;
 float freq = 440;
 
-// Stolen from https://gist.github.com/amirrajan/fa6ce9fdc8918e06ca9759c3358e4cd2
+/*
+ * The callback for the sound wave in SDL2
+ * See https://gist.github.com/amirrajan/fa6ce9fdc8918e06ca9759c3358e4cd2
+ *
+ * @param userdata *void Not used, present for compilation purposes
+ * @param stream Uint8* The sound stream
+ * @param len int The length of the stream
+ */
 void callback(void* userdata, Uint8* stream, int len)
 {
     (void) userdata;
@@ -25,18 +32,37 @@ void callback(void* userdata, Uint8* stream, int len)
     }
 }
 
-// See https://en.wikipedia.org/wiki/Piano_key_frequencies
-static inline float get_note(int n)
+/*
+ * Get the frequency of a key in a classical piano.
+ * See https://en.wikipedia.org/wiki/Piano_key_frequencies
+ *
+ * @param n int The key to get the frequency of
+ * @param float The frequency of the key
+ */
+float get_freq(int n)
 {
     return powf(2, (n - 49) / 12.0) * 440;
 }
 
-static inline float pos(float a)
+/*
+ * If a float is negative, make it 0
+ *
+ * @param n float The float to normalize
+ * @return float The normalized float
+ */
+float normalize(float n)
 {
-    return (a > 0.0) ? a : 0.0;
+    return (n > 0.0) ? n : 0.0;
 }
 
-static inline int scc(int c)
+/*
+ * Check the exit code of SDL functions which return integers.
+ * An exit code less than zero indicates an error
+ *
+ * @param c int The exit code to check
+ * @return int The exit code
+ */
+int scc(int c)
 {
     if (c < 0) {
         fprintf(stderr, "error: %s\n", SDL_GetError());
@@ -45,7 +71,14 @@ static inline int scc(int c)
     return c;
 }
 
-static inline void *scp(void *p)
+/*
+ * Check the value of SDL functions which return pointers.
+ * A NULL pointer indicates an error
+ *
+ * @param p *void The pointer to check
+ * @return int The pointer
+ */
+void *scp(void *p)
 {
     if (p == NULL) {
         fprintf(stderr, "error: %s\n", SDL_GetError());
@@ -54,18 +87,47 @@ static inline void *scp(void *p)
     return p;
 }
 
-int main(void)
+/*
+ * Check if anything in a bool array is true
+ *
+ * @param xs *bool The array to check
+ * @param count size_t The number of elements to check
+ * @return bool Whether a true value was encountered
+ */
+bool any(bool *xs, size_t count)
 {
-    scc(SDL_Init(SDL_INIT_VIDEO));
-    SDL_Window *window = scp(SDL_CreateWindow("Synth",
-                                  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                  640, 480, SDL_WINDOW_SHOWN));
+    for (size_t i = 0; i < count; ++i) if (xs[i]) return true;
+    return false;
+}
 
-    SDL_Surface *surface = SDL_GetWindowSurface(window);
+/*
+ * Update the window
+ *
+ * @param window *SDL_Window The window to update
+ * @param surface *SDL_Surface The surface of the window
+ */
+void update(SDL_Window *window, SDL_Surface *surface)
+{
     SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x2B, 0x2B, 0x2B));
     SDL_UpdateWindowSurface(window);
+}
 
-    scc(SDL_Init(SDL_INIT_AUDIO));
+int main(void)
+{
+    // Initialize SDL
+    scc(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO));
+
+    // ----------------
+    // Setup the window
+    SDL_Window *window = scp(SDL_CreateWindow("Synth",
+                                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                              640, 480, SDL_WINDOW_SHOWN));
+
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    update(window, surface);
+
+    // ----------------
+    // Setup the audio
     SDL_AudioSpec spec, aspec;
     SDL_zero(spec);
     spec.freq = 48000;
@@ -75,103 +137,136 @@ int main(void)
     spec.callback = callback;
     spec.userdata = NULL;
 
-    // Open audio
     int id = scc(SDL_OpenAudioDevice(NULL, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE));
 
-    SDL_Event e;
-    bool running = true;
-
+    // ---------------
+    // The event loop
     int octave = 40;            // Default in the fourth octave
+    bool down[13] = {0};        // The keys down
+    bool play = false;          // Whether the sound is playing
+
+    SDL_Event e;                // The event
+    bool running = true;        // Whether the application is running
+
     while (running) {
+        update(window, surface);
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
             case SDL_QUIT: running = false; break;
             case SDL_KEYDOWN:
                 switch (e.key.keysym.sym) {
-                case SDLK_a: // C
-                    freq = pos(get_note(octave + 0));
+                case 'a': // C
+                    down[0] = true;
+                    freq = normalize(get_freq(octave + 0));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_w: // C#
-                    freq = pos(get_note(octave + 1));
+                case 'w': // C#
+                    down[1] = true;
+                    freq = normalize(get_freq(octave + 1));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_s: // D
-                    freq = pos(get_note(octave + 2));
+                case 's': // D
+                    down[2] = true;
+                    freq = normalize(get_freq(octave + 2));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_e: // E'
-                    freq = pos(get_note(octave + 3));
+                case 'e': // D#
+                    down[3] = true;
+                    freq = normalize(get_freq(octave + 3));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_d: // E
-                    freq = pos(get_note(octave + 4));
+                case 'd': // E
+                    down[4] = true;
+                    freq = normalize(get_freq(octave + 4));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_f: // F
-                    freq = pos(get_note(octave + 5));
+                case 'f': // F
+                    down[5] = true;
+                    freq = normalize(get_freq(octave + 5));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_u: // F#
-                    freq = pos(get_note(octave + 6));
+                case 'u': // F#
+                    down[6] = true;
+                    freq = normalize(get_freq(octave + 6));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_j: // G
-                    freq = pos(get_note(octave + 7));
+                case 'j': // G
+                    down[7] = true;
+                    freq = normalize(get_freq(octave + 7));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_i: // A'
-                    freq = pos(get_note(octave + 8));
+                case 'i': // G#
+                    down[8] = true;
+                    freq = normalize(get_freq(octave + 8));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_k: // A
-                    freq = pos(get_note(octave + 9));
+                case 'k': // A
+                    down[9] = true;
+                    freq = normalize(get_freq(octave + 9));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_o: // B'
-                    freq = pos(get_note(octave + 10));
+                case 'o': // A#
+                    down[10] = true;
+                    freq = normalize(get_freq(octave + 10));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_l: // B
-                    freq = pos(get_note(octave + 11));
+                case 'l': // B
+                    down[11] = true;
+                    freq = normalize(get_freq(octave + 11));
                     SDL_PauseAudioDevice(id, 0);
                     break;
 
-                case SDLK_SEMICOLON: // C+
-                    freq = pos(get_note(octave + 12));
-                    SDL_PauseAudioDevice(id, 0);
-                    break;
-
-                case SDLK_0: octave = -8; break;
-                case SDLK_1: octave = 4; break;
-                case SDLK_2: octave = 16; break;
-                case SDLK_3: octave = 28; break;
-                case SDLK_4: octave = 40; break;
-                case SDLK_5: octave = 52; break;
-                case SDLK_6: octave = 64; break;
-                case SDLK_7: octave = 76; break;
-                case SDLK_8: octave = 88; break;
+                case 'z': octave = -8; break;
+                case 'x': octave = 4; break;
+                case 'c': octave = 16; break;
+                case 'v': octave = 28; break;
+                case 'b': octave = 40; break;
+                case 'n': octave = 52; break;
+                case 'm': octave = 64; break;
+                case ',': octave = 76; break;
+                case '.': octave = 88; break;
 
                 case SDLK_q: running = false; break;
-                default: SDL_PauseAudioDevice(id, 1);
                 }
                 break;
-            case SDL_KEYUP: SDL_PauseAudioDevice(id, 1); break;
+
+            case SDL_KEYUP:
+                switch (e.key.keysym.sym) {
+                case SDLK_a: down[0] = false; break;
+                case SDLK_w: down[1] = false; break;
+                case SDLK_s: down[2] = false; break;
+                case SDLK_e: down[3] = false; break;
+                case SDLK_d: down[4] = false; break;
+                case SDLK_f: down[5] = false; break;
+                case SDLK_u: down[6] = false; break;
+                case SDLK_j: down[7] = false; break;
+                case SDLK_i: down[8] = false; break;
+                case SDLK_k: down[9] = false; break;
+                case SDLK_o: down[10] = false; break;
+                case SDLK_l: down[11] = false; break;
+                case SDLK_SEMICOLON: down[12] = false; break;
+                }
+                break;
+            }
+
+            bool new = any(down, 13);
+            if (play != new) {
+                play = new;
+                SDL_PauseAudioDevice(id, !play);
             }
         }
     }
-
 
     SDL_DestroyWindow(window);
     SDL_CloseAudio();
